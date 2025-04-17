dec8303d13969b58b45c8b3068d5fdce21711ed9
Adhith Karthikeyan <24882134+kkadhith@users.noreply.github.com>
2025-04-16 23:23:46 -0700

    Updated supported iOS version

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "AirAlert",
-    platforms: [.iOS(.v15)],
+    platforms: [.iOS(.v17)],
     products: [
         .library(
             name: "AirAlert",
